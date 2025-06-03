7de573c24bb8e088c7e71abc5b5c978dfad9d601
Jan Kalisz <jan.kalisz007@gmail.com>
2025-06-03 13:39:16 +0200

    Projects

--- Angular/servicesNdStuff/src/app/tasks/tasks.service.ts
+++ Angular/servicesNdStuff/src/app/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { Task, TaskStatus } from './task.model';
+import { LoggingService } from '../logging.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,7 @@ import { Task, TaskStatus } from './task.model';
 export class TasksService {
   private tasks = signal<Task[]>([])
   allTasks = this.tasks.asReadonly()
+  private loggingService = inject(LoggingService)
 
   addTask(taskData: {title:string, description:string}){
     const newTask: Task = {
@@ -15,9 +17,11 @@ export class TasksService {
       status: 'OPEN'
     }
     this.tasks.update((oldTasks)=>[...oldTasks, newTask])
+    this.loggingService.log('ADDED TASK WITH TITLE ' + taskData.title)
   }
 
   updateTaskStatus(taskId:string, newStatus:TaskStatus){
     this.tasks.update((oldTasks)=>oldTasks.map((task)=> task.id === taskId ? {...task, status: newStatus} : task))
+    this.loggingService.log('CHANGED STATUS TO ' + newStatus)
   }
 }
